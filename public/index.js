c5f8984d1e7e4c587e58971c7c2c39ac48be30d8
Santiago Gutierrez <santigutierrez14@icloud.com>
2018-11-16 06:18:45 -0500

    Done

--- public/index.js
+++ public/index.js
@@ -1,28 +1,8 @@
-
 $(document).ready(function () {
-    //createDropdownMenu();
+    writeToStatusPanel("Welcome!!!\n");
+    createDropdownMenu();
     populateFileView();
     populateCardView("testCard.vcf");
-});
-
-var i = 0;
-var fileNameTest; 
-//upload new file to the server
-$('#uploadFileNow').on('click', function (e) {
-    e.preventDefault();
-    var form = e.target;
-    var newData = new FormData(form);
-    $.post({
-        type: 'POST',
-        url: 'http://localhost:30439/upload',//change to ip for Node server
-        data: newData,
-        success: function () {
-            writeToStatusPanel("Uploaded new file succesfully.");
-        },
-        fail: function (){
-            alert("Could not upload file.\n");
-        }
-    });
 });
 
 function populateFileView() {
@@ -40,16 +20,16 @@ function populateFileView() {
                     var COL1 = document.createElement('td');
                     var COL2 = document.createElement('td');
                     var COL3 = document.createElement('td');
-                    
-
-                    var downloadLink = document.createElement('a');
-                    downloadLink.setAttribute('href','/uploads/'+data[i].filename);
 
                     var newNumText = document.createTextNode(data[i].filename);
                     var newNumProp = document.createTextNode(data[i].name);
                     var newNumPropValue = document.createTextNode(data[i].numprops);
-                    COL1.append(downloadLink);
-                    COL1.appendChild(newNumText);
+
+                    var downloadLink = document.createElement('a');
+                    downloadLink.appendChild(newNumText);
+                    downloadLink.setAttribute('href','/uploads/'+data[i].filename);
+                    
+                    COL1.appendChild(downloadLink);
                     COL2.appendChild(newNumProp);
                     COL3.appendChild(newNumPropValue);
                     
@@ -76,7 +56,6 @@ function populateCardView(fileName) {
         url: "getVCardProps/"+fileName, // replace this with correct url
         success: function (data) {
             try{
-
                 var counter = 0;
                 for(key in data){
 
@@ -111,34 +90,31 @@ function populateCardView(fileName) {
 
 function createDropdownMenu(){
     $.ajax({
-        type: "get",
-        url: "/uploads",
-        dataType: "application/json",
-        data:data,
+        type: 'GET',
+        url: '/getVCardSummaries',
         success: function (data) {
-            vcardDataJSON = JSON.parse(data);
-            // console.log(vcardDataJSON);
             try{
                 for(i = 0; i < data.length; i++){
-                    console.log("try");
-                    
-                    var dropdownMenu = document.getElementById('#dropMenu');
-                    var link = document.createElement('a');
-                    link.setAttribute('href', "#");
-                    link.appendChild(document.createTextNode(vcardDataJSON[i].filename));
-                    var thisLI = document.createElement('li');
-                    thisLI.appendChild(link); 
-                    dropdownMenu.append(thisLI);
+                    var newMenuItem = document.createElement('button');
+                    newMenuItem.setAttribute('id',data[i].filename);
+                    newMenuItem.setAttribute('class','btn btn-link');
+                    newMenuItem.innerHTML = data[i].filename;
+                    newMenuItem.addEventListener('click',function(){
+                        populateCardView(data[i].filename).bind(data[i].filename);
+                    });
+                    $("#card_view_menu").append(newMenuItem);
                 }
             }catch(e){
-                //
+                writeToStatusPanel("Error with menu.\n");
             }
         }, fail:function () {
             alert("Issue");
         }
     });
 }
-
 function writeToStatusPanel(toBeWritten) {
-    $('#status_output_p').text(toBeWritten);
+    $('#status_output_p').append(toBeWritten);
 }
+$('#clearBTN').click(function(){
+    $('#status_output_p').remove();
+});
